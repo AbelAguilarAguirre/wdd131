0cfce49fa4425f850252825f2af8e8b9e7ae8600
AbelAguilarAguirre <agu22008@byui.edu>
2024-10-07 21:33:47 -1000

    localStorage

---
+++ week05/scripts/bom.js
@@ -0,0 +1,52 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+    input.value = '';
+    input.focus();
+});
+
+let chaptersArray = getChapterList() || [];
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+        input.focus();
+    });
+
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+
+
